223890f587593d8f4409b56498588c15ea9be827
Eugen Istoc <eugenistoc@gmail.com>
2016-03-04 01:30:07 -0500

    Add core controller

--- app/core/controllers/core.controller.js
+++ app/core/controllers/core.controller.js
@@ -1,5 +1,53 @@
 'use strict';
 
-angular.module('app.core').controller('CoreCtrl', function () {
-  console.log("hello from core");
+angular.module('app.core').controller('CoreCtrl', function ($scope, $ionicModal, RegisterFields, LoginFields, Auth) {
+  this.fields = {
+    register: RegisterFields,
+    login: LoginFields
+  };
+
+  this.auth = function (credentials) {
+    Auth.auth(credentials.email, credentials.password).then(function (user) {
+
+    });
+  };
+
+  this.register = function (registration) {
+    Auth.register(registration.email, registration.password, registration.profile).then(function (user) {
+
+    })
+  };
+
+
+  // Login Modal
+  $ionicModal.fromTemplateUrl('core/views/login.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function (modal) {
+    $scope.loginModal = modal;
+  });
+
+  // Register Modal
+  $ionicModal.fromTemplateUrl('core/views/register.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function (modal) {
+    $scope.registerModal = modal;
+  });
+
+  $scope.hideLogin = function () {
+    $scope.loginModal.hide();
+  };
+
+  $scope.showLogin = function () {
+    $scope.loginModal.show();
+  };
+
+  $scope.hideRegister = function () {
+    $scope.registerModal.hide();
+  };
+
+  $scope.showRegister = function () {
+    $scope.registerModal.show();
+  }
 });
