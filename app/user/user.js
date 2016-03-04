c03458631c758bd85433803e2e2c9c59f07a2808
Eugen Istoc <eugenistoc@gmail.com>
2016-03-04 16:17:49 -0500

    Add Logout to sidemenu

--- app/user/user.js
+++ app/user/user.js
@@ -2,5 +2,6 @@
 
 angular.module('app.user', ['app.core'])
   .run(function (SideMenu) {
-    SideMenu.add('Profile', 'app.profile');
+    SideMenu.add('Profile', 'app.profile', 'ion-person');
+    SideMenu.add('Logout', 'core.logout', 'ion-power');
   });
