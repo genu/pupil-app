cbf88fd9899829938e0c6d202592bd9a19b7ec73
Eugen Istoc <eugenistoc@gmail.com>
2016-03-30 01:51:40 -0400

    Update Stamplay adapter

--- app/core/providers/stamplay.adapter.js
+++ app/core/providers/stamplay.adapter.js
@@ -1,16 +1,55 @@
 'use strict';
 
-angular.module('app.core').provider('DSStamplayAdapter', function DSStamplayAdapterProvider() {
+angular.module('app.core').provider('DSStamplayAdapter', function DSStamplayAdapterProvider(DSProvider) {
+  DSProvider.defaults = {
+    idAttribute: '_id'
+    // defaultFilter: function(){
+    //   debugger;
+    // }
+  };
+
   this.defaults = {};
 
-  function DSStamplayAdapter(defaults, service, q) {
+  function DSStamplayAdapter(defaults, service, q, store, config) {
     this.defaults = defaults;
     this.lib = Stamplay;
     this.service = service;
     this.q = q;
+    this.store = store;
+    this.config = config;
 
-    this.lib.init(this.defaults.appid);
+    this.lib.init(defaults.appid);
   }
+
+  DSStamplayAdapter.prototype.logout = function () {
+    var deferred = this.q.defer();
+
+    this.store.remove(window.location.origin + '-jwt');
+
+    deferred.resolve();
+
+    return deferred.promise;
+  };
+
+  DSStamplayAdapter.prototype.login = function (credentials) {
+    return this.service.User.login(credentials);
+  };
+
+  DSStamplayAdapter.prototype.currentUser = function () {
+    var deferred = this.q.defer();
+
+    this.service.User.currentUser().then(function (user) {
+      deferred.resolve(user.user);
+    }).catch(function (error) {
+      deferred.reject(error);
+    });
+
+    return deferred.promise;
+  };
+
+  DSStamplayAdapter.prototype.register = function (credentials) {
+    return this.service.User.signup(credentials);
+  };
 
   DSStamplayAdapter.prototype.getLib = function () {
     return this.lib;
@@ -22,12 +61,15 @@ angular.module('app.core').provider('DSStamplayAdapter', function DSStamplayAdap
 
   DSStamplayAdapter.prototype.find = function (resourceConfig, id, options) {
     var deferred = this.q.defer();
-
-    this.service.Object(resourceConfig.endpoint).get({_id: id}).then(function (data) {
-      deferred.resolve(data.data[[0]]);
-    }).catch(function (error) {
-      deferred.reject(error);
-    });
+    if (resourceConfig.name === 'user') {
+      return this.currentUser();
+    } else {
+      this.service.Object(resourceConfig.endpoint).get({_id: id}).then(function (data) {
+        deferred.resolve(data.data[[0]]);
+      }).catch(function (error) {
+        deferred.reject(error);
+      });
+    }
 
     return deferred.promise;
   };
@@ -35,8 +77,26 @@ angular.module('app.core').provider('DSStamplayAdapter', function DSStamplayAdap
   DSStamplayAdapter.prototype.findAll = function (resourceConfig, params, options) {
     var deferred = this.q.defer();
 
-    this.service.Object(resourceConfig.endpoint).get().then(function (data) {
-      deferred.resolve(data.data);
+    if (resourceConfig.name === 'user') {
+      this.currentUser().then(function (user) {
+        deferred.resolve([user]);
+      })
+    } else {
+      this.service.Object(resourceConfig.endpoint).get(params).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (error) {
+        deferred.reject(error);
+      });
+    }
+
+    return deferred.promise;
+  };
+
+  DSStamplayAdapter.prototype.create = function (resourceConfig, params, options) {
+    var deferred = this.q.defer();
+
+    this.service.Object(resourceConfig.endpoint).save(params).then(function (response) {
+      deferred.resolve(response);
     }).catch(function (error) {
       deferred.reject(error);
     });
@@ -44,12 +104,9 @@ angular.module('app.core').provider('DSStamplayAdapter', function DSStamplayAdap
     return deferred.promise;
   };
 
-  DSStamplayAdapter.prototype.create = function (definition, attrs, options) {
 
-  };
-
-
-  this.$get = function ($stamplay, $q) {
-    return new DSStamplayAdapter(this.defaults, $stamplay, $q);
+  this.$get = function ($stamplay, $q, store, Config) {
+    return new DSStamplayAdapter(this.defaults, $stamplay, $q, store, Config);
   }
-});
+})
+;
