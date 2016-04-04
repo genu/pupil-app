08eba8532f9fc1267ce9f52d261efb86d3bb1b63
Eugen Istoc <eugenistoc@gmail.com>
2016-04-03 22:51:05 -0400

    Add location resource

---
+++ app/core/resources/location.resource.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('app.core')
+  .service('Location', function (DS) {
+    return DS.defineResource({
+      name: 'Location',
+      relations: {
+        hasOne: {
+          school: {
+            localField: 'school',
+            localKey: 'school_id'
+          }
+        }
+      },
+      afterFind: function (resource, attrs, cb) {
+        attrs.school_id = attrs.school_id[0];
+
+        cb(null, attrs);
+      },
+      afterFindAll: function (resource, attrs, cb) {
+        attrs.school_id = attrs.school_id[0];
+
+        cb(null, attrs);
+      },
+      afterUpdate: function (resource, attrs, cb) {
+        // Convert all school references from arrays to single value
+        _.forEach(attrs, function (instance) {
+          instance.school_id = instance.school_id[0];
+        });
+
+        cb(null, attrs);
+      }
+    })
+  }).run(function (Location) {
+});
